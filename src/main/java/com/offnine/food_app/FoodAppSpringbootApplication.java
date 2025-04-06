b322dad5c67af660266d1a7e79a51717969c0fd2
Everest Paudel <paudeleverest09@gmail.com>
2025-04-06 22:54:10 +0545

    Initialize Spring Boot project with Maven wrapper and basic configuration files

---
+++ src/main/java/com/offnine/food_app/FoodAppSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.offnine.food_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodAppSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodAppSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/offnine/food_app/FoodAppSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.offnine.food_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodAppSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
